508fc9d21491a04f4794b74c65af597cdd0d4473
daixiang <wb_xiang.dai@rokid.com>
2017-03-21 07:41:16 +0800

    add device file

---
+++ device/amlogic/xxxx/sample.mk
@@ -0,0 +1,11 @@
+PRODUCT_PACKAGES += \
+					runtime \
+					libbsiren \
+					mic_array.p230 \
+					libr2ssp \
+					libztvad \
+					libztcodec2 \
+					libr2audio \
+					libr2vt \
+					libjsonc \
+					runtime_cmd
