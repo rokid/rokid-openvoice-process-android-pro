3ac9f24082736dc8409136d3be6d42985f92fab6
daixiang <wb_xiang.dai@rokid.com>
2017-03-31 23:36:20 +0800

    modify directory

--- hardware/modules/mic_array/Android.mk
+++ hardware/modules/mic_array/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 ifneq (, $(filter $(TARGET_DEVICE),msm8974))
 include $(LOCAL_PATH)/qcom_msm89xx/build.mk
 else
-include $(LOCAL_PATH)/amlogic_s9xx/build.mk
+include $(LOCAL_PATH)/xxxx/build.mk
 endif
 
 
