e88d23c47e1832a8e16849fc1a53e8c3fa7ee24e
daixiang <wb_xiang.dai@rokid.com>
2017-05-09 10:19:53 +0800

    update

---
+++ build/core/definitions_exts.mk
@@ -0,0 +1,8 @@
+ifndef all-named-files-under
+define all-named-files-under
+$(patsubst ./%,%, \
+  $(shell cd $(LOCAL_PATH) ; \
+          find -L $(2) -name "$(1)" -and -not -name ".*") \
+ )
+endif
+endif
