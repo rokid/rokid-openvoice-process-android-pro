4ab32d338c85de137abcc21085f5e9615541a06a
daixiang <wb_xiang.dai@rokid.com>
2017-05-10 19:32:36 +0800

    update

--- Android.mk
+++ Android.mk
@@ -8,34 +8,36 @@ LOCAL_SRC_FILES := \
 		src/runtime_main.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-		libbinder \
 		libhardware \
+		libbinder \
 		libbsiren \
 		libutils \
-		libgpr \
 		liblog \
-		libgrpc \
-		libgrpc++ \
-		libprotobuf-rokid-cpp-lite \
-		libprotobuf-rokid-cpp-full \
 		libspeech_common \
-		libspeech
+		libspeech \
+		libspeech_asr 
 
-LOCAL_STATIC_LIBRARIES += libjsonc_static
+LOCAL_STATIC_LIBRARIES += libjsonc_static 
 
-OPENVOICE_DIR_DEP = robot/openvoice
+OPENVOICE_DIR_DEP = openvoice
 
 LOCAL_C_INCLUDES += \
 		$(LOCAL_PATH)/include \
 		$(LOCAL_PATH)/hardware/include \
-		$(OPENVOICE_DIR_DEP)/rokid-blacksiren/include \
-		$(OPENVOICE_DIR_DEP)/rokid-blacksiren/thirdparty/libjsonc/include \
-		$(OPENVOICE_DIR_DEP)/rokid-openvoice-sdk/include 
+		$(OPENVOICE_DIR_DEP)/rokid-blacksiren-master/include \
+		$(OPENVOICE_DIR_DEP)/rokid-blacksiren-master/thirdparty/libjsonc/include \
+		$(OPENVOICE_DIR_DEP)/speech/include
 
-IGNORED_WARNINGS := -Wno-sign-compare -Wno-unused-parameter -Wno-sign-promo -Wno-error=return-type -Wno-error=non-virtual-dtor
+ifeq ($(PLATFORM_SDK_VERSION), 18)
+LOCAL_SHARED_LIBRARIES += libdl
+LOCAL_STATIC_LIBRARIES += libc++
+LOCAL_C_INCLUDES += external/libcxx/include
+endif
+
 LOCAL_MODULE := runtime
+#LOCAL_CXX_STL := libc++
 LOCAL_ARM_MODE := arm
-LOCAL_CPPFLAGS := -DCURRENT_ANDROID_PLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) $(IGNORED_WARNINGS) -DGOOGLE_PROTOBUF_NO_RTTI
+LOCAL_CPPFLAGS := -DCURRENT_ANDROID_PLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) -std=c++11
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_TARGET_ARCH := arm
 
