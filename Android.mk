945d9f24cc653bef10fd3e1997606017d6f88741
daixiang <wb_xiang.dai@rokid.com>
2017-05-11 15:22:44 +0800

    update

--- Android.mk
+++ Android.mk
@@ -33,9 +33,13 @@ LOCAL_SHARED_LIBRARIES += libdl
 LOCAL_STATIC_LIBRARIES += libc++
 LOCAL_C_INCLUDES += external/libcxx/include
 endif
+ifeq ($(PLATFORM_SDK_VERSION), 19)
+LOCAL_SDK_VERSION := 14
+LOCAL_NDK_STL_VARIANT := gnustl_static
+endif
+#protostream_objectwriter.cc
 
 LOCAL_MODULE := runtime
-#protostream_objectwriter.cc
 LOCAL_ARM_MODE := arm
 LOCAL_CPPFLAGS := -DCURRENT_ANDROID_PLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) -std=c++11
 LOCAL_MODULE_TAGS := optional
