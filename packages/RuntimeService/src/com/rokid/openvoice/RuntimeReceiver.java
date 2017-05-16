97871dcfc28da8829c42f10461bd7c1800e8244d
daixiang <wb_xiang.dai@rokid.com>
2017-05-16 14:09:27 +0800

    update

--- packages/RuntimeService/src/com/rokid/openvoice/RuntimeReceiver.java
+++ packages/RuntimeService/src/com/rokid/openvoice/RuntimeReceiver.java
@@ -1,9 +1,11 @@
 package com.rokid.openvoice;
 
+import android.content.Intent;
+
 public class RuntimeReceiver extends android.content.BroadcastReceiver{
 
 	@Override
-	public void onReceive(android.content.Context context, android.content.Intent intent){
+	public void onReceive(android.content.Context context, Intent intent){
 		String action = intent.getAction();
 		android.util.Log.e("RuntimeReceiver", "onReceive : " + action);
 		if(Intent.ACTION_BOOT_COMPLETED.equals(action)){
@@ -12,7 +14,7 @@ public class RuntimeReceiver extends android.content.BroadcastReceiver{
 			android.net.NetworkInfo mNetworkInfo = intent.getParcelableExtra(android.net.ConnectivityManager.EXTRA_NETWORK_INFO);
 			if(mNetworkInfo != null){
 				android.util.Log.e("RuntimeReceiver", mNetworkInfo.toString());
-				if(mNetworkInfo.isConneted()){
+				if(mNetworkInfo.isConnected()){
 					RuntimeNative.asInstance().networkStateChange(true);
 				}else{
 					RuntimeNative.asInstance().networkStateChange(false);
