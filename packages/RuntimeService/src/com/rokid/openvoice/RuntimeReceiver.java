6bb652d2d203723428a5de0e21e1100cd77c9b2d
daixiang <wb_xiang.dai@rokid.com>
2017-05-16 13:40:25 +0800

    update

--- packages/RuntimeService/src/com/rokid/openvoice/RuntimeReceiver.java
+++ packages/RuntimeService/src/com/rokid/openvoice/RuntimeReceiver.java
@@ -1,24 +1,22 @@
 package com.rokid.openvoice;
 
-import android.content.Intent;
-import android.net.NetworkInfo;
-import android.net.NetworkInfo.DetailedState;
-
 public class RuntimeReceiver extends android.content.BroadcastReceiver{
 
 	@Override
-	public void onReceive(android.content.Context context, Intent intent){
+	public void onReceive(android.content.Context context, android.content.Intent intent){
 		String action = intent.getAction();
 		android.util.Log.e("RuntimeReceiver", "onReceive : " + action);
 		if(Intent.ACTION_BOOT_COMPLETED.equals(action)){
 			context.startServiceAsUser(new Intent(context, RuntimeService.class), android.os.UserHandle.OWNER);
-		}else if(android.net.wifi.WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(action)){
-			NetworkInfo info = intent.getParcelableExtra(android.net.wifi.WifiManager.EXTRA_NETWORK_INFO);
-			DetailedState state = info.getDetailedState();
-			if(state == DetailedState.CONNECTED){
-				RuntimeNative.asInstance().networkStateChange(true);
-			}else if(state == DetailedState.DISCONNECTED){
-				RuntimeNative.asInstance().networkStateChange(false);
+		}else if(android.net.ConnectivityManager.CONNECTIVITY_ACTION.equals(action)){
+			android.net.NetworkInfo mNetworkInfo = intent.getParcelableExtra(android.net.ConnectivityManager.EXTRA_NETWORK_INFO);
+			if(mNetworkInfo != null){
+				android.util.Log.e("RuntimeReceiver", mNetworkInfo.toString());
+				if(mNetworkInfo.isConneted()){
+					RuntimeNative.asInstance().networkStateChange(true);
+				}else{
+					RuntimeNative.asInstance().networkStateChange(false);
+				}
 			}
 		}
 	}
