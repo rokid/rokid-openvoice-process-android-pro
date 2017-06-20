a4614f56f05963f36870c3085ca0dbec9b8e3c94
daixiang <xiang.dai@rokid.com>
2017-06-20 20:28:33 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -2,51 +2,114 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
 #include <binder/Parcel.h>
 #include <binder/IServiceManager.h>  
 
+using namespace std;
 using namespace android;
+sp<IBinder> proxy = defaultServiceManager()->getService(String16("runtime_native")); 
+
+void init(){
+    Parcel data, reply;
+    data.writeInterfaceToken(String16(DESCRIPTOR));
+    proxy->transact(IBinder::FIRST_CALL_TRANSACTION, data, &reply); 
+    reply.readExceptionCode();
+}
+
+void start_siren(int flag){
+    Parcel data, reply;
+    data.writeInterfaceToken(String16(DESCRIPTOR));
+    data.writeInt32(flag);
+    proxy->transact(IBinder::FIRST_CALL_TRANSACTION + 1, data, &reply); 
+    reply.readExceptionCode();
+}
+
+void set_siren_state(int flag){
+    Parcel data, reply;
+    data.writeInterfaceToken(String16(DESCRIPTOR));
+    data.writeInt32(flag);
+    proxy->transact(IBinder::FIRST_CALL_TRANSACTION + 2, data, &reply); 
+    reply.readExceptionCode();
+}
+
+void network_state_change(int flag){
+    Parcel data, reply;
+    data.writeInterfaceToken(String16(DESCRIPTOR));
+    data.writeInt32(flag);
+    proxy->transact(IBinder::FIRST_CALL_TRANSACTION + 3, data, &reply); 
+    reply.readExceptionCode();
+}
+
+void automatic_test(int count){
+    if(count < 0) return;
+    while(count--){
+        int index = rand() % 6;
+        printf("============%d============\n", index);
+        switch(index) {
+            case 0:
+                start_siren(0);
+                break;
+            case 1:
+                start_siren(1);
+                break;
+            case 2:
+                set_siren_state(0);
+                break;
+            case 3:
+                set_siren_state(1);
+                break;
+            case 4:
+                network_state_change(0);
+                break;
+            case 5:
+                network_state_change(1);
+                break;
+            case 6:
+                init();
+                break;
+        }
+        sleep(2);
+    }
+    start_siren(1);
+    network_state_change(1);
+}
 
 int main(int argc, char** argv){
     if(argc < 2){
-        printf("%s\n", "needs two arguments");
+        printf("Needs two arguments");
         return 0;
     }
-    sp<IBinder> proxy = defaultServiceManager()->getService(String16("runtime_native")); 
     Parcel data, reply;
     printf("%s\n", argv[1]);
     if(proxy != NULL && proxy->isBinderAlive()){
         if(strcmp(argv[1], "init") == 0){
-            data.writeInterfaceToken(String16(DESCRIPTOR));
-            proxy->transact(IBinder::FIRST_CALL_TRANSACTION, data, &reply); 
-            reply.readExceptionCode();
+            init();
         }else if(strcmp(argv[1], "start") == 0){
             if(argc < 3){
-                printf("%s\n", "needs three arguments");
+                printf("Needs three arguments");
                 return 0;
             }
-            data.writeInterfaceToken(String16(DESCRIPTOR));
-            data.writeInt32(atoi(argv[2]));
-            proxy->transact(IBinder::FIRST_CALL_TRANSACTION + 1, data, &reply); 
-            reply.readExceptionCode();
+            start_siren(atoi(argv[2]));
         }else if(strcmp(argv[1], "state") == 0){
             if(argc < 3){
-                printf("%s\n", "needs three arguments");
+                printf("Needs three arguments");
                 return 0;
             }
-            data.writeInterfaceToken(String16(DESCRIPTOR));
-            data.writeInt32(atoi(argv[2]));
-            proxy->transact(IBinder::FIRST_CALL_TRANSACTION + 2, data, &reply); 
-            reply.readExceptionCode();
+            set_siren_state(atoi(argv[2]));
         }else if(strcmp(argv[1], "netchg") == 0){
             if(argc < 3){
-                printf("%s\n", "needs three arguments");
+                printf("Needs three arguments");
                 return 0;
             }
-            data.writeInterfaceToken(String16(DESCRIPTOR));
-            data.writeInt32(atoi(argv[2]));
-            proxy->transact(IBinder::FIRST_CALL_TRANSACTION + 3, data, &reply); 
-            reply.readExceptionCode();
+            network_state_change(atoi(argv[2]));
+        }else if(strcmp(argv[1], "auto") == 0){
+            if(argc < 3){
+                printf("Please enter the number of times");
+                return 0;
+            }
+            automatic_test(atoi(argv[2]));
         }
     }else{
         printf("%s\n", "proxy is null");
