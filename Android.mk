f43add3fef9e02816ce1c27de240a8ad6538722c
daixiang <xiang.dai@rokid.com>
2017-07-27 12:57:48 +0800

    update

--- Android.mk
+++ Android.mk
@@ -4,35 +4,35 @@ include $(CLEAR_VARS)
 $(shell cp $(LOCAL_PATH)/etc/openvoice_profile.json $(TARGET_OUT_ETC))
 
 LOCAL_SRC_FILES := \
-	src/IVoiceService.cpp \
-	src/VoiceService.cpp \
-	src/Engine.cpp \
-	src/main.cpp
+			src/IVoiceService.cpp \
+			src/VoiceService.cpp \
+			src/Engine.cpp \
+			src/main.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-	libhardware \
-	libbinder \
-	libbsiren \
-	libutils \
-	liblog \
-	libspeech
+			libhardware \
+			libbinder \
+			libbsiren \
+			libutils \
+			liblog \
+			libspeech
 
 LOCAL_STATIC_LIBRARIES += libjsonc_static
 
 LOCAL_C_INCLUDES += \
-					$(LOCAL_PATH)/include \
-					$(LOCAL_PATH)/hardware/include \
-					$(LOCAL_PATH)/../rokid-blacksiren/include \
-					$(LOCAL_PATH)/../rokid-blacksiren/thirdparty/libjsonc/include
+			$(LOCAL_PATH)/include \
+			$(LOCAL_PATH)/hardware/include \
+			$(LOCAL_PATH)/../rokid-blacksiren/include \
+			$(LOCAL_PATH)/../rokid-blacksiren/thirdparty/libjsonc/include
 
 ifeq ($(PLATFORM_SDK_VERSION), 22)
-	LOCAL_SHARED_LIBRARIES += libc++ libdl
-	LOCAL_C_INCLUDES += external/libcxx/include
+LOCAL_SHARED_LIBRARIES += libc++ libdl
+LOCAL_C_INCLUDES += external/libcxx/include
 else ifeq ($(PLATFORM_SDK_VERSION), 19)
-	LOCAL_C_INCLUDES += \
-						external/stlport/stlport \
-						bionic
-	LOCAL_STATIC_LIBRARIES += libstlport_static
+LOCAL_C_INCLUDES += \
+			external/stlport/stlport \
+			bionic
+LOCAL_STATIC_LIBRARIES += libstlport_static
 endif
 
 LOCAL_MODULE := openvoice_proc
