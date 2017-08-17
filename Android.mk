8bb46672e7494d04104d9cf062dbafc0c11c01fc
daixiang <xiang.dai@rokid.com>
2017-08-17 22:46:32 +0800

    update

--- Android.mk
+++ Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
 $(shell cp $(LOCAL_PATH)/etc/openvoice_profile.json $(TARGET_OUT_ETC))
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
