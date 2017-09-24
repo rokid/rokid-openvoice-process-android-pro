b73eb96b0066348e0604e90d75c088651143e21f
daixiang <xiang.dai@rokid.com>
2017-09-24 14:14:55 +0800

    update

--- frameworks/native/services/openvoice_process/Android.mk
+++ frameworks/native/services/openvoice_process/Android.mk
@@ -56,13 +56,13 @@ LOCAL_SHARED_LIBRARIES += libc++ libdl
 LOCAL_C_INCLUDES += external/libcxx/include
 else ifeq ($(PLATFORM_SDK_VERSION), 19)
 MY_LOCAL_STATIC_LIBRARIES := prebuilts/ndk/current/sources/cxx-stl/gnu-libstdc++/libs/$(TARGET_CPU_ABI)/libgnustl_static.a
-LOCAL_LDFLAGS += "-Wl,--start-group" $(MY_LOCAL_STATIC_LIBRARIES) "-Wl,--end-group" -ldl
+LOCAL_LDFLAGS += $(MY_LOCAL_STATIC_LIBRARIES) -ldl
+LOCAL_STATIC_LIBRARIES += libstlport_static
 LOCAL_C_INCLUDES += \
 		prebuilts/ndk/current/sources/cxx-stl/gnu-libstdc++/libs/$(TARGET_CPU_ABI)/include \
 		prebuilts/ndk/current/sources/cxx-stl/gnu-libstdc++/include \
 		external/stlport/stlport \
 		bionic
-LOCAL_STATIC_LIBRARIES += libstlport_static
 endif
 
 LOCAL_MODULE := openvoice_proc
